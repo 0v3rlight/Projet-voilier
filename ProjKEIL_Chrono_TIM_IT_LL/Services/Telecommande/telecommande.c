084914ac8074f168d620fb66f60e2c66cc615a2c
Assa Diarra <diarra@insat.insa-toulouse.fr>
2019-11-14 12:16:11 +0100

    Telecommande s2

--- ProjKEIL_Chrono_TIM_IT_LL/Services/Telecommande/telecommande.c
+++ ProjKEIL_Chrono_TIM_IT_LL/Services/Telecommande/telecommande.c
@@ -7,7 +7,8 @@ Utilise la lib MyTimers.h /.c
 */
 
 
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "Chrono.h"
 #include "MyTimer.h"
 #include "telecommande.h"
@@ -16,7 +17,8 @@ Utilise la lib MyTimers.h /.c
 void config_gpio(){
 	
 	//enable clock gpio
-	
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
 	
 	//PB6 en floating input
 	LL_GPIO_SetPinMode(GPIOB,LL_GPIO_PIN_6,LL_GPIO_MODE_FLOATING);
@@ -39,15 +41,37 @@ void init_timer_pwm_input(TIM_TypeDef * Timer){
 	if (Timer == TIM3)	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 	if (Timer == TIM4)	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
 	
+/*
+	• Select the active input for TIMx_CCR1: write the CC1S bits to 01 in the TIMx_CCMR1
+register (TI1 selected).
+• Select the active polarity for TI1FP1 (used both for capture in TIMx_CCR1 and counter
+clear): write the CC1P to ‘0’ (active on rising edge).
+• Select the active input for TIMx_CCR2: write the CC2S bits to 10 in the TIMx_CCMR1
+register (TI1 selected).
+• Select the active polarity for TI1FP2 (used for capture in TIMx_CCR2): write the CC2P
+bit to ‘1’ (active on falling edge).
+• Select the valid trigger input: write the TS bits to 101 in the TIMx_SMCR register
+(TI1FP1 selected).
+• Configure the slave mode controller in reset mode: write the SMS bits to 100 in the
+TIMx_SMCR register.
+• Enable the captures: write the CC1E and CC2E bits to ‘1 in the TIMx_CCER register.
+	*/
+	
+	Timer->ARR = 0xFFFF;
+	Timer->PSC = 0x0100;
+	
+	Timer->CR1 |= TIM_CR1_CEN;
+	
+	
 	Timer->CCMR1 |= TIM_CCMR1_CC1S_0;
 	Timer->CCMR1 &= ~TIM_CCMR1_CC1S_1;
 	
-	Timer->CCR1 &= TIM_CCER_CC1P;
+	Timer->CCER &= ~TIM_CCER_CC1P;	
 	
 	Timer->CCMR1 &= ~TIM_CCMR1_CC2S_0;
 	Timer->CCMR1 |= TIM_CCMR1_CC2S_1;
 	
-	Timer->CCR2 |= TIM_CCER_CC2P; 
+	Timer->CCER |= TIM_CCER_CC2P; 
 	
 	Timer->SMCR |= TIM_SMCR_TS_0;
 	Timer->SMCR &= ~TIM_SMCR_TS_1;
@@ -55,25 +79,21 @@ void init_timer_pwm_input(TIM_TypeDef * Timer){
 	
 	Timer->SMCR &= ~TIM_SMCR_SMS_0;
 	Timer->SMCR &= ~TIM_SMCR_SMS_1;
-	Timer->SMCR |= ~TIM_SMCR_SMS_2;
+	Timer->SMCR |= TIM_SMCR_SMS_2;
 	
 	Timer->CCER |= TIM_CCER_CC1E;
 	Timer->CCER |= TIM_CCER_CC2E;
 }
 
 int get_period(TIM_TypeDef * Timer){
-	return(Timer->CCR2);	
+	return(Timer->CCR2);	//per
 }
 
-int get_angle(int per){
-	
-	int angle = 360*per - 540;
-	
-	//traitement angle
-	
-	return(angle);
-	
-	
-	
+int get_angle(int per){	
+	return(360*per - 540);	
+}
+
+float get_vitesse(int angle){	
+	return((5/9)*abs(angle));	
 }
 
